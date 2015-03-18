c06f8a388672c7e36099b2774013dc0084371afd
Administrator <jmrphyii@memphis.edu>
2015-03-18 13:32:43 -0500

    update

--- mips problem two.asm
+++ mips problem two.asm
@@ -1,1 +1,211 @@
+#Resources used
+#http://www.cplusplus.com/reference/cstdlib/atoi/
+#http://www.cmi.ac.in/~nivedita/subjects/CO/mips-material/atoi-3.asm
+#
+.data
+			.align 2
+	array:		.space	1024
+	fileinput:	.asciiz	"PA2_input2.txt"
+	fileoutput:	.asciiz	"PA2_output3.txt"
+	bufferin:	.space	1024
+	bufferout:	.space	1024
 	
+	
+.text
+
+# Open file
+	li 	$v0, 13
+	la 	$a0, fileinput
+	li 	$a1, 0
+	li 	$a2, 0
+	syscall
+	move 	$s6, $v0
+	
+	# Read data
+	li 	$v0, 14
+	move 	$a0, $s6
+	la 	$a1, bufferin
+	li 	$a2, 1024
+	syscall	
+	
+	# Close file
+	li $v0, 16
+	move $a0, $s6
+	syscall
+	
+#conversion to ints
+
+startatoi:
+	la $s0, array		# s0 contains aray
+	move $a0, $a1		# a0 contains buffer address
+	j atoi				# convert
+	
+atoi:
+	#zero out the to registers v0 and t0
+	move $v0, $zero
+	move $t0, $zero
+	#first step is to load a byte to convert
+	lb $t0, 0($a0)		#desintaion of byte is t0
+	
+checkSign:
+	bne $t0, 45, findValue		#check wether ot no t0 equals a - sign
+	#if so we need to flag it
+	addi $t3, $zero, 1
+	#if it is negative then advance the buffer 
+	addi $a0, $a0, 1
+	
+findValue:
+	lb $t0, 0($a0)		#we loaded a byte into $t0
+	#now we check to make sure that the byte is not a int
+	#it can be a new line or a null terminator
+	beq $t0, 10, checkpn	
+	beq $t0, 0, endConversion
+	
+		
+#check the flag to make sure that is is not negative if it will then go to store int
+#if it is negative by negative one 
+checkpn:
+	bne $t3, 1, store
+	mul $t1, $t1, -1
+	move $t3, $zero
+	j store
+	
+	
+store:
+	sw $t1, 0($s0) 		#storing the integer into the array for sorting
+	move $t1, $zero		#clear out results of previous stored intry
+	addi $s0, $s0, 4	#to increase the an array in mips we need to increase the overall counter
+				#by four.
+	addi $a0, $a0, 1	#the buffer is byte by byte adressable so we only need to increment it by
+				#to get the next byte
+	move $s1, $t2		#move the current count of ints 
+	addi $t2, $t2, 1 	#increase the total count by one
+	
+endConversion:
+	move	$t2, $zero
+	move	$s5, $s1
+	mul	$s1, $s1, 4
+	
+
+
+#sorting time
+#One thing I am happy about is not quick sort
+sort:
+#pre limary steps
+	la $t0, array		#load array into t0
+	add  $t0, $t0, $s1 	#set up counter
+	
+#this would be like the outer loop for sorting
+outter:
+	#for(int i=0; i < t0; i++)
+	add $t1, $0, $0
+	la $t0, array 		#load array into t0
+inner:	
+	lw $t2, 0($t2) 		#Loads the first element of the array I
+	lw $t2, 0($t2)		# loads the next element in the array for comparison
+				#tto sort the array
+	slt $t4, $t3, $t2	#checks whether or not the i+1 element is less than i
+	beq $t4, $0, switchelemets	#if ^ true then a flag turns to one
+	add $t1, $0, 1
+	sw  $t2, 4($a0)		# t2 =  i + 1
+    	sw  $t3, 0($a0)		# t3 = i
+	
+
+switchelemets:
+	addi $a0, $a0, 4
+	bne $a0, $t0, inner
+	bne $t1, $0, outter
+	j itoa
+	
+
+#itoa
+
+itoa:
+	la $a0, array
+	move $a1, $s5
+	la $a2, bufferout
+startitoa:
+	lw $t0, 0($a0)		#load element from array
+	move $t1, $zero		#zero out t1
+	addi $t1, $t1, 1	#set t1 to the starting magnitude
+	move $t6, $t0		#further calculations
+	
+findMag:
+	div $t6, $t6, 10 	#see if wee need to increase the magnitude by dividing by 10
+	#we need to know have a condition to see if it is less than 10 when dividing
+	beq $t6, 0, checksigns	#checks to see if it is eaual to zero
+	addi $t1, $t1, 1 	#increse the magnitude of the value
+	
+checksigns:
+	move 	$t6, $zero
+	bgt 	$t0, -1, positive	# if the value is greater than -1 that means that it is a positive value
+					#know if it is not greater than then we go to negative
+	addi	$t6, $t6, '-'	
+	sb 	$t6, 0($a2)		# write bufferout
+	addi 	$a2, $a2, 1		# increment buffer
+	mul 	$t0, $t0, -1		# (-9)(-1)=9
+	
+
+
+positive:
+	move $t2, $a0
+	move $t3, $a1
+	move $a0, $zero
+	addi $a0, $a0, 10
+	move 	$a1, $t1
+	move 	$t5, $zero		
+	addi 	$t5, $t5, 1		
+	
+	
+exponent:
+	mul $t5, $t5, $a0		#
+	addi $a1, $a1, -1 		#decrement magnitdue
+	bgt  $a1, 1, exponent	# if a1 is gretare than one 
+	move 	$a0, $t2		
+	move 	$a1, $t3
+	
+loop:
+	div $t6, $t0, $t5		# get left most byte
+	mul $t4, $t6, $t5		# find mag
+	sub $t0, $t0, $t4		# -result
+	addi $t6, $t6, '0'		# acsii
+	div $t5, $t5, 10		#reset mag
+	sb $t6, 0($a2)			#store byte in buffer
+	addi $a2, $a2, 1		#need to increase the byte locarion in buffer
+	bne $t5, 0, loop		#if t5 is not 0 continue with loop
+	
+	move $t6, $zero
+	addi $t6, $t6, 10 		#t6 is the new line
+	sb $t6, 0($a2)
+	addi $a2, $a2, 1		#buffer++
+	addi 	$a1, $a1, -1		#num int --
+	beqz 	$a1, nullTerm		#
+	addi 	$a0, $a0, 4		# increment the array by four bytes
+	j startitoa
+	
+			
+nullTerm:		
+	move $t6, $zero
+	addi $t6, $t6, 0	
+	sb $t6, 0($a2)		#store byte
+	
+	
+	li 	$v0, 13			# opens fileoutput
+	la 	$a0, fileoutput		# load file descriptor
+	li 	$a1, 1			# flag for writing
+	li 	$a2, 0			# ignore
+	syscall				# makes it happen
+	move 	$s7, $v0		# $s7 == file descriptor
+	
+	
+	
+	li 	$v0, 15			# setup for writing to fileoutput
+	move 	$a0, $s7		# $a0 == file descriptor
+	la 	$a1, bufferout		# $a1 == bufferout address
+	li 	$a2, 1024		# bufferout size in bytes
+	syscall				# writes to file
+	
+	
+	exit:	
+	li 	$v0, 10			# setup for exiting gracefully
+	syscall		
--- part3.asm
+++ part3.asm
@@ -4,7 +4,7 @@
 	buffer:	.space 1024
 	
 	intro: 	.ascii "This is my linked list"
-	outb:	.space 1024
+	outboundBuffer:	.space 1024
 	first:	.asciiz ""
 	
 	outro: 	.ascii ". The end of the list is  "
@@ -13,7 +13,9 @@
 	.align 2
 	array:	.space 1024
 	
-.text
+	
+	.text
+	
 	#a0 buffer
 	li $v0, 13 	# Open file service
 	la $a0, file	# Load 'file' into arg1
@@ -32,113 +34,107 @@
 	move $a0, $s7	# File descriptor from before
 	syscall
 	
-	la $a0, buffer	# Load the source buffer into arg1
-	la $a1, array	# Load the destination array into arg2
-	jal atoi	# Parse the strings to ints and put them into the array
+	#load both the array and buffer addresses for later use
+	la $a0, buffer
+	la $a1, array
+	jal itoa
 	
-	la $a0, array	# Loads the address of our linkedlist-array
-	la $a2, outb	# Locates the output buffer into arg3
-	jal linkedlist	# Runs the parser that traverses the linked list itself.
+	#after converting the ints to the aray we need to reload the arrayand
+	#load the outbound buffer to display text in file
+	la $a0, array	
+	la $a2, outboundBuffer
+	#need to jump to the linkedlist portion to format linked list
+	jal LinkedList
 	move $s3, $v1	# Saves the position that the terminating element was found at.
 	
-	li $v0, 13	# Open file service
-	la $a0, outf	# Loads 'outf' into arg1
-	li $a1, 1	# For writing
-	li $a2,	0	# Ignored
-	syscall
-	move $s2, $v0	# Saves the file descriptor
+	outBoundFilePrep:
+		li $v0, 13		# Open file service
+		la $a0, outf		# Loads 'outf' into arg1
+		li $a1, 1		# For writing
+		li $a2,	0		# Ignored
+		syscall
+		move $s2, $v0		# Saves the file descriptor
 	
-	li $v0, 15	# Write file service
-	move $a0, $s2	# File descriptor
-	la $a1, intro	# Buffer to write
-	move $a2, $s4	# Number characters to write
-	addi $a2, $a2, 25
-	syscall
+		li $v0, 15		# Write file service
+		move $a0, $s2		# File descriptor
+		la $a1, intro		# Buffer to write
+		move $a2, $s4		# Number characters to write
+		addi $a2, $a2, 25
+		syscall
+		
+		
+		move $s4, $zero		# Resets the number characters used
+		
+		move $a0, $s3		# Parses the position index
+		la $a2, pos		# Loads the buffer to print
+		jal itoa		# Parses it
 	
-	move $s4, $zero	# Resets the number characters used
+		li $v0, 15		# file service
+		move $a0, $s2		# File descriptor
+		la $a1, outro		# Moves the output string to the arg
+		move $a2, $zero		# Zeroes it
+		addi $a2, $a2, 44	# Num characters to write
+		add $a2, $a2, $s4	# Including the number we used.
+		syscall
 	
-	move $a0, $s3	# Parses the position index
-	la $a2, pos	# Loads the buffer to print
-	jal itoa	# Parses it
-	
-	li $v0, 15	# file service
-	move $a0, $s2	# File descriptor
-	la $a1, outro	# Moves the output string to the arg
-	move $a2, $zero	# Zeroes it
-	addi $a2, $a2, 44	# Num characters to write
-	add $a2, $a2, $s4	# Including the number we used.
-	syscall
-	
-	li $v0, 10
-	syscall
+		#end program
+		li $v0, 10
+		syscall
+		
 	
 	
-linkedlist:
-	## $a0 is the array containing the linked list
-	## $a2 is the output buffer to write the list to
-	## $v0 is the number of characters used
-	## $v1 is the index of the terminating character (-1)
-	## $s4 is the number of characters used until returned
-	## $t0 is the current number
-	## $t1 is the current node's next-node pointer's address
-	## $t2 is the next-node pointer's value
-	## $t6 is a writing register
-	## $t7 is a copy of the array
-#################################
-	move $s7, $ra		#
-	jal zerotemps		#
-	move $ra, $s7		#
-	move $t7, $a0		#
-itr:	lw $t0, 0($t7)		# Loads an element
-	move $s0, $a0		# Saves our original $a0
-	move $a0, $t0		# Moves our to-be-written int to the arguments
-	move $s7, $ra	
-	jal pushvars		# Stores our $t* registers
+	
+LinkedList:
+	move $s7, $ra
+	move $ra, $s7		
+	move $t7, $a0
+
+	iteration: lw $t0, 0($t7)	#load an element out of the copy of the array
+	move $s0, $a0 			#save the array adress from argument into another register 
+	move $a0, $t0			#move the current number into the argument resister
+	move $s7, $ra
+	jal push 			#push the temp register to the stack 
 	move $ra, $s7
 	move $s7, $ra
-	jal itoa		# Write that element
+	jal itoa			#convert all the ints to ascsii chars
 	move $ra, $s7
 	move $s7, $ra
-	jal fetchvars		# Fetches the $t* registers
+	jal pop				#pop all the elements off of the stack
 	move $ra, $s7
-	move $a0, $s0		# Resets our arg1
-	add $s4, $s4, $v0	# Accumulates our running total of characters used
-	addi $t1, $t7, 4	# Sets $t1 to the current element's next-node pointer's address
-	lw $t2, 0($t1)		# Fetches the value of the next-node pointer.
-	beq $t2, -1, endlist	# If the next-node pointer is -1, we are done.
-	#			#
-	move $t6, $zero		# Zeroes the register
-	addi $t6, $t6, '-'	# Adds the formatted character
+	move $a0, $s0			#make a0 available
+	add $s4, $s4, $v0		#gather the runing count of the chars
+	addi $t1, $t7, 4		#get the next element that is available for use
+	lw $t2, 0($t1)			#load the next node for use
+	beq $t2, -1, reachedTheEnd		#if the next node is -1 the we are done
+	
+	#if t2 is not -1 then we do this. We need to place the - char and the >before we print the nest
+	#element 
+	move $t6, $zero		# make s6 0
+	addi $t6, $t6, '-'	# adds one of the number seperators to t6
 	sb $t6, 0($a2)		# Writes the character
 	addi $a2, $a2, 1	# Advances the write buffer
 	addi $s4, $s4, 1 	# Increments the character counter
-	##			## These two blocks are for printing the formatted output characters
-	move $t6, $zero		# ^
-	addi $t6, $t6, '>'	# ^
-	sb $t6, 0($a2)		# ^
-	addi $a2, $a2, 1	# ^
-	addi $s4, $s4, 1 	# ^
-	#			#
-	mul $t2, $t2, 4		# Converts the index in $t2 to an offset
-	add $t7, $a0, $t2	# Moves the current-node pointer to where the next-node pointer indicated
-	j itr			#
-				#
-endlist:			#
-	sub $v1, $t1, $a0	# Obtains the terminating character's offset
-	div $v1, $v1, 4		# Obtains an index from the offset.
-	move $v0, $s6		# Puts the number of used characters into the return register.
-	jr $ra			#
-#################################
-
-
-atoi:
-	## $a0 is the input string's address
-	## $a1 is the output array's address
-	## $v0 is the address of the output array
-	## $v1 is the number of elements in the output array
-	## $t7 is the copied string address, $t6 is the final number, $t5 is the negative flag
-	## $t0 is the current byte, $t4 is the number of elements
-#################################	
+	#same code but writing the > char
+	move $t6, $zero		# make s6 0
+	addi $t6, $t6, '>'	# adds one of the number seperators to t6
+	sb $t6, 0($a2)		# Writes the character
+	addi $a2, $a2, 1	# Advances the write buffer
+	addi $s4, $s4, 1 	# Increments the character counter	
+	#this will in total form the -> string that goes between each number 
+	
+	mul $t2, $t2, 4	
+	add $t7, $a0, $t2
+	j iteration
+	#once we have reached the end of the list by reaching the terminating character then we have reached the end of the list
+	reachedTheEnd:
+		sub $v1, $t1, $a0
+		div $v1, $v1, 4
+		move $v0, $s6 # puts the number of chars into v0
+		jr $ra	
+	
+	
+	
+atoi:	
 	move $t7, $a0 		# Copying the input string's address so we can manipulate it.
 	move $v0, $zero		#
 reset:	lb $t0, 0($t7)		# Fetching the first byte, Also our starting point
@@ -175,110 +171,89 @@ parsedone:			#
 	move $v0, $a1		# Moves the output array to the results register
 	move $v1, $t4		# Stores the number of elements in the array
 	jr $ra			#
-#################################
+
 
 
 itoa:
-	## $a0 is the int to convert
-	## $a2 is the buffer to write to
-	## $v0 is the number of characters used so far
-	## $t1 is the current number
-	## $t2 is the magnitude of the current element
-	## $t3 is the number of characters used so far
-	## $t7 is the current byte to write
-#################################
-start:	move $t1, $a0		# Loads the current element in the array
-	move $t2, $zero		# Makes sure $t2 is zero.
-	addi $t2, $t2, 1	# Sets our magnitude indicator to 1 to start with.
-	move $t7, $t1		# Removes one extra label by doing the temporary register here.
+start:	move $t1, $a0		# Loading aka moving the current byte to be used
+	move $t2, $zero		
+	addi $t2, $t2, 1	# Smagnitude begins at one
+	move $t7, $t1	
 	j magfind		# Finds the magnitude of the current number
 	move $t7, $zero		# Resets our $t7 for future use.
-negative:			#
-	bgt $t1, -1, positive	# Skips the negative writing if it's positive or zero
-	addi $t7, $t7, '-'	# Stores the negative in the writing register
-	sb $t7, 0($a2)		# Writes the byte to the buffer
-	addi $t3, $t3, 1	# Increments the characters used so far
-	addi $a2, $a2, 1	# Advances the write buffer one byte
-	mul $t1, $t1, -1	# Since we have written the negative, we want to make our number positive.
-positive:			#
-	move $t4, $a0		# Saves our original arg1
-	move $a0, $zero		# Zeroes arg1
-	addi $a0, $a0, 10	# Sets it to 10
-	move $a1, $t2		# Sets arg2 to the magnitude of the number
-	move $t6, $zero		# Zeroes $t6
-	addi $t6, $t6, 1	# Sets it to 1 so exp can work.
-	j exp			#
+negative:			
+	bgt $t1, -1, positive	
+	addi $t7, $t7, '-'	# if the value is negative it stroes it in the register that writes
+	sb $t7, 0($a2)		# it will load the byte into the buffer
+	addi $t3, $t3, 1	# chars used +1
+	addi $a2, $a2, 1	# +1 write buffer
+	mul $t1, $t1, -1	# since the number is negative we need to make is positive
+				#using the format of (NegativeNum * -1) = the number in its positive value
+positive:			
+	move $t4, $a0		#moving and saving 
+	move $a0, $zero		
+	addi $a0, $a0, 10	# a0 = 10
+	move $a1, $t2		# move the magnitude of the number to the a0 register
+	move $t6, $zero		
+	addi $t6, $t6, 1	
+	j exp			
 eret:	move $a0, $t4		# Restores original values
-notdone:			#
-	div $t7, $t1, $t6	# Gets the leftmost digit of our number to our writing byte
-	mul $t5, $t7, $t6	# Gets the adjusted value (e.g. 600 out of 628) from writing byte * mag to $t5
-	sub $t1, $t1, $t5	# Gives us the other digits (e.g. 28 out of 628) from number - (writing byte* mag) to $t1
-	addi $t7, $t7, '0'	# Gets the ASCII value to writing byte
-	div $t6, $t6, 10	# Reduces our magnitude register by a factor of 10 (e.g. from 100 to 10)
-	sb $t7, 0($a2)		# Writes the current byte
+notdone:			
+	div $t7, $t1, $t6	#get the left most digit to our writing buffer
+	mul $t5, $t7, $t6	#Get the true value of the int get the closest number
+				#place for the number if your number is 1555 then it would 
+				#get 1000 
+	sub $t1, $t1, $t5	#get the other value (555 out of 1555)
+	addi $t7, $t7, '0'	
+	div $t6, $t6, 10	#reduce the magnitude register by dividing by 10
+	sb $t7, 0($a2)		#writes the byte to the buffer
 	addi $t3, $t3, 1	# Increments the characters used so far
 	addi $a2, $a2, 1	# Advances the write buffer one byte
-	bne $t6, 0, notdone	# If we have magnitude left, branch to notdone.
-	j return		# We've finished.
-				#
-				#
-	# $t2 is the magnitude	#
-				#
-magfind:			#
-	div $t7, $t7, 10 	# Divides the number by ten
-	beq $t7, 0, negative	# If we got 0, it means our number < 10, so we're done.
-	addi $t2, $t2, 1	# Else, we increment our magnitude indicator, then go again
-	j magfind		# ^
-				#
-	# Base is in $a0	#
-	# Power is in $a1	#
-	# Result is in $v0	#
-exp:	beq $a1, 1, nil		#
-	mul $t6, $t6, $a0	#
-	addi $a1, $a1, -1	#
-	bgt $a1, 1, exp		#
-nil:	j eret			#
-				#
+	bne $t6, 0, notdone	#as long as the magnitude still exist then we need 
+				#to branch to notdon again until we do not ave any
+				#magnitude left
+	j return		
+	
+	magfind:
+	div $t7, $t7, 10 	#we need to divide the number that is strored in t7(the vurrent byte)
+				# this allows us to tell wether or not out number that we have is less thatn
+				#10.
+	beq $t7, 0, negative
+	addi $t2, $t2, 1	
+	j magfind		
+	
+	
+exp:	beq $a1, 1, nil		
+	mul $t6, $t6, $a0	
+	addi $a1, $a1, -1	
+	bgt $a1, 1, exp		
+nil:	j eret			
+				
 return:	move $v0, $t3		# This is the number of chracters that was used.
 	jr $ra			# Returns
-#################################
 
-zerotemps:
-######################### This method simply zeroes all our temporary registers so we can be sure they're free.
-	move $t0, $zero	#
-	move $t1, $zero #
-	move $t2, $zero #
-	move $t3, $zero #
-	move $t4, $zero #
-	move $t5, $zero #
-	move $t6, $zero #
-	move $t7, $zero #
-	jr $ra		#
-#########################
 
-pushvars:
-# Tstores all variable on the stacke
-	addi $sp, $sp, -48	#
-	sw $8, 16($sp)		#
-	sw $9, 20($sp)		#
-	sw $10, 24($sp)		#
-	sw $11, 28($sp)		#
-	sw $12, 32($sp)		#
-	sw $13, 36($sp)		#
-	sw $14, 40($sp)		#
-	sw $15, 44($sp)		#
-	jr $ra			#
-#################################	
+push:
+	addi $sp, $sp, -48 	#need to make room for the variables
+	sw $8, 16($sp)		#store with offset starting at 16 and increasing by four each time
+	sw $9, 20($sp)		
+	sw $10, 24($sp)		
+	sw $11, 28($sp)		
+	sw $12, 32($sp)		
+	sw $13, 36($sp)		
+	sw $14, 40($sp)		
+	sw $15, 44($sp)		#^
+	jr $ra			
 
-fetchvars:
-# fetches all variables on the stack
-	lw $8, 16($sp)		#
-	lw $9, 20($sp)		#
-	lw $10, 24($sp)		#
-	lw $11, 28($sp)		#
-	lw $12, 32($sp)		#
-	lw $13, 36($sp)		#
-	lw $14, 40($sp)		#
-	lw $15, 44($sp)		#
-	addi $sp, $sp, 48	#
-	jr $ra			#
+
+pop:
+	lw $8, 16($sp)		#nneed to store all the variable back into their locations
+	lw $9, 20($sp)		
+	lw $10, 24($sp)		
+	lw $11, 28($sp)		
+	lw $12, 32($sp)		
+	lw $13, 36($sp)		
+	lw $14, 40($sp)		
+	lw $15, 44($sp)		
+	addi $sp, $sp, 48 	#pops all elements off of stack
+	jr $ra			
