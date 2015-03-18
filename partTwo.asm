4ec406d281786047ce3357a043575d8bf97ca4b8
Michael Vance <mvance1@memphis.edu>
2015-03-18 12:57:03 -0500

    First comment/upload

--- partTwo.asm
+++ partTwo.asm
@@ -55,55 +55,54 @@ special:				# Did not implement negatives, or number larger than single digits
 	addi	$t0, $t0, 1		# This basically skips those instances. . .
 	j continue
 
-# Sort Data
 sort:
-     la		$t0, array      	# Copy the base address of your array into $t1
-     add 	$t0, $t0, 1024    	                            
-outterLoop:             		# Used to determine when we are done iterating over the Array
-     add 	$t1, $0, $0     	# $t1 holds a flag to determine when the list is sorted
-     la  	$a0, array      	# Set $a0 to the base address of the Array
-innerLoop:                  		# The inner loop will iterate over the Array checking if a swap is needed
-     lb  	$t2, 0($a0)         	# sets $t2 to the current element in array
-     lb  	$t3, 1($a0)         	# sets $t3 to the next element in array
-     slt 	$t5, $t2, $t3       	# $t5 = 1 if $t2 < $t3
-     beq 	$t5, $0, contin  	# if $t5 = 1, then swap them
-     add 	$t1, $0, 1          	# if we need to swap, we need to check the list again
-     sb  	$t2, 1($a0)         	# store the greater numbers contents in the higher position in array (swap)
-     sb  	$t3, 0($a0)         	# store the lesser numbers contents in the lower position in array (swap)
-contin:
-     addi 	$a0, $a0, 1            	# advance the array to start at the next location from last time
-     bne  	$a0, $t0, innerLoop    	# If $a0 != the end of Array, jump back to innerLoop
-     bne  	$t1, $0, outterLoop    	# $t1 = 1, another pass is needed, jump back to outterLoop
- 
-# Print Data
-#print:	li	$t2, 5
-#	add  	$t1, $zero, $t2  # initialize loop counter to array size
-#	la	$s4, array
+ la $a1,array      #load a pointer to array into $a1 
 
-#out_print:
-#	li	$v0, 1		# Print String Syscall
-#	lb	$a0, 0($s4)	# Load Contents String
-#	syscall
-	
-#	la  	$a0, space       # load address of spacer for syscall
-#	li   	$v0, 4           # specify Print String service
-#	syscall               # print the spacer string
-	
-#	addi 	$s4, $s4, 1      # increment address of data to be printed
-#	addi 	$t1, $t1, -1     # decrement loop counter
-#	bgtz 	$t1, out_print         # repeat while not finished
+loop:
+ li $t1,20          #if $t1 is zero, load 9 into $t1
+
+loop1: 
+ beqz $t1,convertBack     #if $t2 is zero, goto here 
+ addi $t1,$t1,-1   #subtract 1 from $t2, save to $t2 
+ lb $t5,0($a1)     #load an input int into $t5 
+ lb $t6,1($a1)     #load the next one into $t6 
+ addi $a1,$a1,1    #add 4 to $a1, save to $a1 
+ ble $t5,$t6,loop1 #if $t5 <= $t6, goto loop1 
+ sb $t5,0($a1)     #else, store $t5 in $a1 
+ sb $t6,-1($a1)     #and store $t6 in $a1-4 (swapping them) 
+ bgez  $t1,loop1    #if $t2 is not zero, to go loop1
+
+# Sort Data
+#sort:
+#     la		$t0, array      	# Copy the base address of your array into $t1
+#     add 	$t0, $t0, 1024    	                            
+#outterLoop:             		# Used to determine when we are done iterating over the Array
+#     add 	$t1, $0, $0     	# $t1 holds a flag to determine when the list is sorted
+#     la  	$a0, array      	# Set $a0 to the base address of the Array
+#innerLoop:                  		# The inner loop will iterate over the Array checking if a swap is needed
+#     lb  	$t2, 0($a0)         	# sets $t2 to the current element in array
+#     lb  	$t3, 1($a0)         	# sets $t3 to the next element in array
+#     slt 	$t5, $t2, $t3       	# $t5 = 1 if $t2 < $t3
+#     beq 	$t5, $0, contin  	# if $t5 = 1, then swap them
+#     add 	$t1, $0, 1          	# if we need to swap, we need to check the list again
+#     sb  	$t2, 1($a0)         	# store the greater numbers contents in the higher position in array (swap)
+#     sb  	$t3, 0($a0)         	# store the lesser numbers contents in the lower position in array (swap)
+#contin:
+#     addi 	$a0, $a0, 1            	# advance the array to start at the next location from last time
+#     bne  	$a0, $t0, innerLoop    	# If $a0 != the end of Array, jump back to innerLoop
+#     bne  	$t1, $0, outterLoop    	# $t1 = 1, another pass is needed, jump back to outterLoop
 
 # Convert Data Back
 convertBack:			# Basically trying to undo all of the previous conversions to get sorted buffer
 	la	$t7, buffer	# Load address of buffer
 	la	$t0, array	# Load address of array
-	la	$t2, newline	# Load address of newline
+#	lb	$t2, newline	# Load address of newline
 continueAgain:	
 	lb	$t1, 0($t0)	# Load byte (int from array)
 	beq	$t1, 0, close	# If the end of array, go ahead and close
 	addi	$t1, $t1, 48	# Int -> ASCII
 	sb	$t1, 0($t7)	# Store byte (char into buffer)
-	sb	$t2, 1($t7)	# Add a newline
+#	sb	$t2, 1($t7)	# Add a newline
 	addi	$t7, $t7, 1	# Increment buffer
 	addi	$t0, $t0, 1	# Increment array
 	j continueAgain		# repeat loop
