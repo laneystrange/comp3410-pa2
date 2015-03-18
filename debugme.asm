8cd1bfb2f5a5aaa1020200500e82f53b3179246f
Michael Vance <mvance1@memphis.edu>
2015-03-18 12:01:49 -0500

    Second comment/upload

--- debugme.asm
+++ debugme.asm
@@ -1,3 +1,10 @@
+# COMP3410
+# Author: Michael Vance
+# Assignment: PA[2]
+# Date: 3/14/2015
+
+# partOne.asm
+
 .data
             welcome:  .asciiz "Welcome to the mysterious MIPS Program\n"
             msg_iterate: .asciiz "Iteration: "
@@ -9,35 +16,42 @@
                       .word 39        
                       
  .text
-            la $v0, welcome
+            la $a0, welcome
             jal printstr
     
             la $s0, first
-            ori $s4, 0x0
+            ori $s4, 0
             move $s1, $zero
             
-loop: 	slt $s2, $s1, 0x03
+            li $t0, 3
+loop:	slt $s2, $s1, $t0
             beq $s2, $zero, end
             lw $s3, 0($s0)
            
             add $s4, $s4, $s3
-            add $s0, $s0, 0x4
+            add $s0, $s0, 4
             addi $s1, $s1, 1
             la $a0, msg_iterate
             jal printstr
             
             move $a0, $s1
-            jal printstr
+            jal printint
            
             la $a0, linebreak
             jal printstr
             
             j loop
             
-end: move $4, $20
-            jal writeint
-            j _exit
+end:	move $a0, $s4
+            jal printint
+            j exit
             
 printstr:	li $v0, 4
 		syscall
 		jr $ra
+printint:	li $v0, 1
+		syscall
+		jr $ra
+
+exit:	li $v0, 10
+	syscall
