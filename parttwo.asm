f5f7ef766e5e2e732da164b9f5634f264292c346
Claire Skaggs <ephemeralsylph@gmail.com>
2015-03-19 14:29:52 -0500

    More parttwo.asm

--- parttwo.asm
+++ parttwo.asm
@@ -34,7 +34,7 @@ la $a0, input_buffer		# store the memory address of 'input_buffer' into $a0
 la $a1, array			# store the memory address of 'array' into $a1
 jal ttoz			# jump to the 'ttoz' function then return
 jal atoi			# jump to the 'atoi' function then return
-move $s0, $v1			# store the number of integers in the array to $s0 
+move $s0, $v0			# store the number of integers in the array to $s0 
 
 move $a0, $s0			# store the number of integers in the array to $a0
 jal ttoz			# jump to the 'ttoz' function then return
@@ -64,128 +64,119 @@ li $v0, 10			# store the integer value for exit, 10, into $v0
 syscall				# issue the exit syscall
 
 ##################################################################
-# atoi - converts ASCII values to Integer values and stores them #
+# atoi - converts ascii values to integer values and stores them #
 ##################################################################
+# converts ascii values to integer values into an array		 # 
 # ARGUMENTS:							 #
-# $a0 = memory address of the buffer				 #
-# $a1 = memory address of the array				 #
+# $a0 = memory address of buffer				 #
+# $a1 = memory address of array					 #
 # RETURNS:							 #
-# $v0 = the array of converted integers to be returned		 #
-# $v1 = the number of integers in the array to be returned	 #
+# $v0 = number of integers in array				 #
 ##################################################################
 atoi:
-move $t0, $a0			# $t0 is a copy of the buffer memory address in $a0
-
-atoi_begin:
-lb $t1, 0($t0)			# $t1 is the current byte of $t0
-move $t4, $zero			# $t4 is the negative flag and is set to 0
-bne $t1, 45, atoi_loop		# if $t1 does not point to a negative sign (ASCII 45), jump to 'atoi_loop', else continue
+lb $t1, 0($a0)			# $t1 = current char from buffer
+move $t3, $zero			# $t3 = order of mag
+move $t4, $zero			# $t4 = neg flag
+bne $t1, 45, atoi_loop		# if current char != negative sign character, jump to 'atoi_loop'
 
 atoi_neg:
-addi $t4, $t4, 1		# $t4 is the negative flag and is set to 1
-addi $t0, $t0, 1		# $t0 is shifted by 1 byte to bypass the negative sign
+addi $t4, $t4, 1		# neg flag = 1
+addi $a0, $a0, 1		# advance the buffer by 1 byte
 
 atoi_loop:
-lb $t1, 0($t0)			# $t1 is the current byte of $t0
-beq $t1, 10, atoi_prestore	# if $t1 points to a newline feed (ASCII 10), jump to 'atoi_prestore', else continue
-beq $t1, 0, atoi_prestore	# if $t1 points to a null terminator (ASCII 0), jump to 'atoi_prestore', else continue
-subi $t1, $t1, 0x30		# convert the ASCII value of the character to the decimal
-mul $t3, $t3, 10		# for each digit increase the magitute by one order
-add $t3, $t3, $t0		# add the converted value of the current byte
-addi $t0, $t0, 1		# $t0 is shifted by 1 byte to point to the next character
-j atoi_loop			# loop back to gather every digit until $t1 points to either a newline feed or a null terminator
+lb $t1, 0($a0)			# $t1 = current char from buffer
+beq $t1, 10, atoi_prestore	# if current char == '\n' character, jump to 'atoi_loop'
+beq $t1, 0, atoi_prestore	# if current char == null terminator character, jump to 'atoi_loop'
+sub $t1, $t1, '0'		# converts current char to a decimal value
+mul $t3, $t3, 10		# order of mag = order of mag * 10
+add $t3, $t3, $t1		# $t3 = order of mag * 10 + current char
+addi $a0, $a0, 1		# advance the buffer by 1 byte
+j atoi_loop
 
 atoi_prestore:
-bne $t4, 1, atoi_store		# if $t4 is not flagged, then jump to 'atoi_store', else continue
+bne $t4, 1, atoi_store		# if neg flag == 0, then jump to 'atoi_store'
 mul $t3, $t3, -1		# multiply the number by -1 to ensure its value is negative when stored
 
 atoi_store:
-sw $t3, 0($a1)			# store the newly converted integer, t3, into memory at array address, $a1
+sw $t3, 0($a1)
 addi $a1, $a1, 4		# shift the array address, $a1,  forward by 4 bytes
 addi $t2, $t2, 1		# increment the number of integers in the array, $t2, by 1
-addi $t0, $t0, 1		# shift the buffer address copy, $t0,  forward by 1 byte
-move $t3, $zero			# set the converted integer, $t3, to 0
-beq $t1, 0, atoi_end		# if $t1 points to a null terminator, then jump to 'atoi_end', else continue
-j atoi_begin			# start again as we have not yet hit a null terminator
+addi $a0, $a0, 1		# shift the buffer forward by 1 byte
+move $t3, $zero			# $t3 = 0
+beq $t1, 0, atoi_end		# if current char == null terminator character, then jump to 'atoi_end'
+j atoi
 
 atoi_end:
-move $v0, $a1			# store the array address, $a1, to the return address, $v0
-move $v1, $t2			# store the number of integers in the array, $t2, to the return address, $v1
+move $v0, $t2			# store the number of integers into $v0 for return
 jr $ra				# return to the line where the 'atoi' function was called
 
 ##################################################################
-# itoa - converts Integer values to ASCII values and stores them #
+# itoa								 #
 ##################################################################
+# converts an array of integer values to ascii values 		 #
 # ARGUMENTS:							 #
-# $a0 = memory address of the array of integers to convert	 #
-# $a1 = number of integers stored in the array $a0		 #
-# $a2 = memory address of the space to place the result integers #
+# $a0 = memory address of array					 #
+# $a1 = number of integers in array				 #
+# $a2 = memory address of output buffer				 #
 ##################################################################
 itoa:
-move $t0, $a0			# copy the memory address of the integer array $a0 into $t0
-
-itoa_begin:
-move $t1, $zero			# make sure $t1 is 0
-lw $t1, 0($t0)			# load the first integer from the array
-move $t2, $zero			# $t2 is the magnitude counter, reset it to 0
-addi $t2, $t2, 1		# $t2 is the magnitude counter, initalize it to 1
-move $t3, $zero			# make sure $t3 is 0
-move $t3, $t1			# copy the value of $t1 into $t3 so it its magnitude may be found without altering it
-j itoa_mag			# call the 'itoa_mag' function to set the magnitude counter correctly
+lw $t0, 0($a0)			# $t0 = current int from array
+move $t7, $t0			# $t7 = current int from array
+move $t1, $zero			# $t1 = 0
+addi $t1, $t1, 1		# $t1 = mag count, initially 1
 
 itoa_mag:
-div $t3, $t3, 10		# divide the integer value stored in $t3 by ten
-beq $t3, $zero, itoa_neg	# if the integer is now < 10, jump to 'itoa_neg', else continue
-addi $t2, $t2, 1		# increment the magnitude counter by 1
-j itoa_mag			# loop until its magnitude is counted
+div $t7, $t7, 10		# current int = current int / 10
+beq $t7, 0, itoa_sign		# if current int == 0, then jump to 'itoa_sign'
+addi $t1, $t1, 1		# mag count = mag count + 1
+j itoa_mag
 
-itoa_neg:
-move $t3, $zero			# set $t3 back to 0
-move $t4, $zero			# set $t4 back to 0
-move $t5, $zero			# set $t4 back to 0
-move $t4, $t2			# save a copy of the magnitude counter $t2 into $t4 for 'itoa_exp' function
-li $t5, 1			# store the value of 1 into $t5 for 'itoa_exp' function
-bgt $t1, -1, itoa_exp		# if $t1 > -1, jump to 'itoa_exp', else continue
-addi $t3, $t3, '-'		# store the negative sign character to $t3
-sb $t3, 0($a2)			# store the negative sign character in $t3 to the buffer $a2
-addi $a2, $a2, 1		# shift the buffer memory address in $a2 forward 1 byte
-mul $t1, $t1, -1		# the negative sign is already written to the buffer, so make $t1 postive
-move $t3, $zero			# set $t3 back to 0
+itoa_sign:
+move $t7, $zero			# $t7 = 0
+bgt $t0, -1, itoa_pos		# if current int > -1, then jump to 'itoa_pos'
+addi $t7, $t7, '-'		# $t7 = '-' character
+sb $t7, 0($a2)			# store the '-' character to the buffer
+addi $a2, $a2, 1		# advance the buffer by 1 byte
+mul $t0, $t0, -1		# make the current integer postive now
+move $t7, $zero			# $t7 = 0
+
+itoa_pos:
+move $t2, $zero			# $t2 = 0
+move $t3, $zero			# $t3 = 0
+move $t4, $zero			# $t4 = 0
+addi $t2, $t2, 10		# $t2 = 10
+addi $t3, $t3, 1		# $t3 = 1, will be order of mag
+move $t4, $t1			# $t4 = mag count
 
 itoa_exp:
-beqz $t4, itoa_loop		# if $t4, a copy of the magnitude counter = 0, then jump to 'itoa_loop' else continue.
-mul $t5, $t5, 10		# repeatly multiply 10 times itself, storing that into result to $t5
-addi $t4, $t4, -1		# decrement the magnitude counter copy
-j itoa_exp			# loop until the value of $t5 is the proper value
+mul $t3, $t3, $t2		# $t3 = order of mag * 10
+addi $t4, $t4, -1		# mag count = mag count - 1
+bgt $t4, 1, itoa_exp		# if mag count > 1, jump to 'itoa_exp'
 
 itoa_loop:
-beqz $t2, itoa_store		# if the magnitude counter = 0, then jump to 'itoa_store', else continue
-move $t7, $zero			# set $t7 to zero
-div $t1, $t5			# $t1 / 10 ^ magnitude counter
-mflo $t1			# store $t1 % 10 ^ magnitude counter result into $t1
-mfhi $t7			# store $t1 / 10 ^ magnitude counter result into $t7
-addi $t7, $t7, 0x30		# convert this value from integer to ASCII
-sb $t7, 0($a2)			# store this converted value from $t7 to the current byte of the buffer, $a2
-addi $a2, $a2, 1		# shift the buffer memory address in $a2 forward 1 byte
-addi $t2, $t2, -1		# decrement the magnitude counter by -1
-j itoa_loop			# make another pass until we run out of magnitude counter
+div $t7, $t0, $t3		# $t7 = left-most digit
+mul $t6, $t7, $t3		# $t6 = left-most digit * order of mag
+sub $t0, $t0, $t6		# $t0 = integer - (left-most digit * order of mag)
+div $t3, $t3, 10		# decrease order of mag by one order
+addi $t7, $t7, '0'		# converts the left-most digit to ascii
+sb $t7, 0($a2)			# store the converted left-most digit to the buffer
+addi $a2, $a2, 1		# advance the buffer by 1 byte
+bne $t3, 0, itoa_loop		# if order of mag != 0, then jump to 'itoa_loop'
 
-itoa_store:
-addi $t1, $t1, 0x30		# convert this value from integer to ASCII
-sb $t1, 0($a2)			# store this converted value from $t1 to the current byte of the buffer, $a2
-addi $a2, $a2, 1		# shift the buffer memory address in $a2 forward 1 byte
-move $t1, $zero			# make sure $t1 is zero
-addi $t1, $t1, 10		# add the ASCII value for the newline character to $t1
-sb $t1, 0($a2)			# store this newline character from $t1 to the current byte of the buffer, $a2
-addi $a2, $a2, 1		# shift the buffer memory address in $a2 forward 1 byte
-addi $a1, $a1, -1		# decrement the number of integers left by 1
-beq $a1, 0, itoa_end		# if we are out of integers, jump to 'itoa_end', otherwise continue
-j itoa_begin
+itoa_newline:
+move $t7, $zero 		# $t7 = 0
+addi $t7, $t7, 10		# $t7 = '\n' character
+sb $t7, 0($a2) 			# store the '\n' character to the buffer
+addi $a2, $a2, 1		# advance the buffer by 1 byte
+addi $a1, $a1, -1		# num of ints = num of ints - 1
+beqz $a1, itoa_null		# if num of ints == 0, then jump to 'itoa_null'
+addi $a0, $a0, 4		# advance the array by 4 bytes
+j itoa
 
-itoa_end:
-move $t1, $zero			# make sure $t1 is 0
-addi $t1, $t1, 0		# add the ASCII value for the null terminator chracter to $t1
-sb $t1, 0($a2)			# store this newline character from $t1 to the current byte of the buffer, $a2
+itoa_null:
+move $t7, $zero 		# $t7 = 0
+addi $t7, $t7, 0		# $t7 = null terminator character
+sb $t7, 0($a2) 			# store the null terminator character to the buffer
 jr $ra				# return to the line where the 'itoa' function was called
 
 ##################################################################
