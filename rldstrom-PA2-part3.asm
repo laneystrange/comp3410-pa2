dc58ded3e75b23e2463da19fd60b4f4ad2b38fa7
Pronis <redstrom83@gmail.com>
2015-03-19 05:04:12 -0500

    Finishing part3 for fun

--- rldstrom-PA2-part3.asm
+++ rldstrom-PA2-part3.asm
@@ -94,32 +94,49 @@ exitatoi:
 #####################################################################################################################################	
 	
 #####################################################################################################################################
+
+# Need to implement a stack, check data flow, and recheck data flow, and again recheck data flow. 
+
 linkedlist:
-	
-        la	$a0, array		# $a0 set to the address of the array
+	addi	$sp, $sp, -8 		# finish implementing the stack
+	la	$a0, array		# $a0 set to the address of the array
         add	$a0, $a0, $s1		# add the total number of bytes to $a0, now we have a reference to the end of the LL.
         la	$s0, array		# $s0 set to the beginning of the array
         add	$t7, $zero, $s0		# $t7 temp variable pointer to the beginning of the array
         add	$t6, $zero, $a0		# $t6 temp variable pointer to the end of the array
         
         lw	$t0, 0($s0)		# $t0 == first integer a[1]
-        lw	$t1, 4($s0)		# $t1 == second integer, i,e, index 0's pointer
-        move 	$t3, $t1		# $t3 == index 0s pointer
-        addi	$t6, $t6, -4		# this will end up deleted, testing
-        lw	$t5, ($t6)		# extract the value located at the address $t6 was pointing to
-        addi	$t6, $t6, 4		# increment the address $t6 is pointing to, more testing will be deleted
-        sub	$t4, $t6, $t7		# sub'd $t6 and $t6, got 28 the total bytes located in the array, more testing will be del'd
+        move	$s7, $t0		# store the first integer to be stored on the stack
         
-loop:					# main logic from the testing above
-	slti	$s2, $t3, 1		# if $t3 i.e. the pointer of index 0, is less than 1 then flag $s2
-	addi 	$t3, $t3, -1		# else decrement $t3 by one
+        lw	$t1, 4($s0)		# $t1 == second integer, i,e, index 0's pointer
+        addi	$t1, $t1, -1		# decrement the pointer by one because arrays start from 0
+        move 	$t3, $t1		# $t3 will be a temp holder for the pointer for further calculations
+        
+        j loop				# go see what the pointer is pointing at
+        
+dowork:
+	lw	$s7, ($t7)		# $s7 is set to the value in the array that $t7 points to
+	add	$t7, $zero, $s0		# reset the address of the array i.e. start from array[0]
+	addi	$s7, $s7, -1		# decrement the pointer
+	move	$t3, $s7		# $t3 is the argument for the loop
+	
+        
+loop:					# iterates through our array starting at index[0] through each iteration
+	addi 	$t3, $t3, -1		# decrement $t3 by one
+	addi	$t7, $t7, 4		# increment the array base address forward one index through each loop
 	beqz	$t3, store		# if $t3 == 0 then we have our next variable to store
-	addi	$t7, $t7, 4		# else increment $t7 the pointer to the beginning of the array by 4 bytes
-	bnez 	$t3, loop		# this will probably go to, if $t3 != 0 then were not done increment the array
+	bnez 	$t3, loop		# if $t3 != 0 then were not done increment the array
 	
 store:
-	lw	$s5, ($t7)		# then the value will end up getting passed here to itoa function and written to the buffer
+	lw	$s6, ($t7)		# $s6 will hold the value to be sent to itoa
+	beq	$s6, -1, pop		# if $s6 == -1 then we are at the end of the linked list, start poppin the stack
+	move	$t0, $s6		# $t0 is set to the value to be written, $t0 is also the argument of itoa
+       	j dowork			# else there's more work to do
+    
+pop:
+	# Implementing a stack to hold the linked list
 	
+
          
         #NOTE: My apologies, I hope you will at least look through the logic to tell me if I was heading to a dead end.
         
@@ -131,11 +148,11 @@ store:
 # Converts integers to ascii values
 
 itoa:
-	move	$a1, $s5		# move the total count into $a1
+	move	$t2, $s5		# move the total count into $a1
 	la	$a2, buffer		# load the address of the outputbuffer
 
 initialize_itoa:	
-	move 	$t0, $t2		# load integer in $t0
+#	move 	$t0, $t2		# load integer in $t0
 	move 	$t1, $zero		# zero-out $t1
 	addi 	$t1, $t1, 1		# defalut magnitude set to 1
 	move 	$t6, $t0		# $t6 temp register for further calculations
@@ -185,7 +202,7 @@ loopie:
 	sb 	$t6, 0($a2)		# write \n
 	addi 	$a2, $a2, 1		# increment the bufferout by one byte
 	addi 	$a1, $a1, -1		# decrement the total count of integers to be written
-	beqz 	$a1, addnullterm	# if a1 == 0, branch to addnullterm
+	beqz 	$a1, openwritefile	# if a1 == 0, branch to addnullterm
 	addi 	$a0, $a0, 4		# increment the array by four bytes
 	j initialize_itoa		
 
@@ -208,8 +225,6 @@ writetofile:
 	la 	$a1, buffer		# $a1 == bufferout address
 	li 	$a2, 1024		# bufferout size in bytes
 	syscall	
-	move	$ra, $t3				# writes to file
-	jr	$ra
 	
 exit:	
 	li 	$v0, 10			# setup for exiting gracefully
