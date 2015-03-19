82ce4ceb31d542db40eec0e6d5eebdd3cd47b3ba
Will Robb <wmrobb@memphis.edu>
2015-03-18 20:46:52 -0500

    First

--- debugme.asm
+++ debugme.asm
@@ -1,3 +1,6 @@
+#Will Robb
+#pa2-1
+
 .data
             welcome:  .asciiz "Welcome to the mysterious MIPS Program\n"
             msg_iterate: .asciiz "Iteration: "
@@ -9,14 +12,14 @@
                       .word 39        
                       
  .text
-            la $v0, welcome
+            la $a0, welcome	#fixed v0 to a0
             jal printstr
     
             la $s0, first
             ori $s4, 0x0
             move $s1, $zero
             
-loop: 	slt $s2, $s1, 0x03
+loop: 	slti $s2, $s1, 4	#fixed stl to stli and 0x3 to 4
             beq $s2, $zero, end
             lw $s3, 0($s0)
            
@@ -30,13 +33,22 @@ loop: 	slt $s2, $s1, 0x03
             jal printstr
            
             la $a0, linebreak
+            #jal printstr	#fixed need to use print int
+            li $v0, 1
+            syscall
+            
+            la $a0, linebreak
             jal printstr
             
             j loop
             
 end: move $4, $20
-            jal writeint
-            j _exit
+            #jal writeint	#fixed 
+            #j _exit
+            li $v0, 1
+            syscall
+            li $v0, 10
+            syscall
             
 printstr:	li $v0, 4
 		syscall
